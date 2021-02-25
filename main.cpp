af66583fcaa0a35111e212f7f3d60db20d361771
Subhanil Ghosh <sg271223@gmail.com>
2021-02-25 17:21:36 +0530

    cost changed

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    cout << "Cost of pen " << 30 << endl;
-    cout << "Cost of pencil " << 15 << endl;
-    cout << "Cost of eraser " << 8;
+    cout << "Cost of pen " << 40 << endl;
+    cout << "Cost of pencil " << 20 << endl;
+    cout << "Cost of eraser " << 10;
     return 0;
 }
