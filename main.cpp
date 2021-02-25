17cbdcb4c4102432644e3490013784ecd92e2539
Subhanil Ghosh <sg271223@gmail.com>
2021-02-25 16:52:10 +0530

    endl removed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main() {
     cout << "Cost of pen " << 30 << endl;
     cout << "Cost of pencil " << 15 << endl;
-    cout << "Cost of eraser " << 8 << endl;
+    cout << "Cost of eraser " << 8;
     return 0;
 }
