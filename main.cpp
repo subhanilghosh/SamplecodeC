9f517532b3b80524007bf1855a3180e2e1a86c8b
Subhanil Ghosh <sg271223@gmail.com>
2021-02-25 16:44:52 +0530

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Cost of pen " << 30 << endl;
+    cout << "Cost of pencil " << 15 << endl;
+    cout << "Cost of eraser " << 8 << endl;
+    return 0;
+}
