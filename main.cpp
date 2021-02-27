b382d4e851ce552cc0c023ecb4967735a286bad4
Subhanil Ghosh <sg271223@gmail.com>
2021-02-27 15:34:32 +0530

    Variable created to store discount value

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main() {
-    cout << "Cost of pen " << 40 * 0.7 << endl;
-    cout << "Cost of pencil " << 20 * 0.7 << endl;
-    cout << "Cost of eraser " << 10 * 0.7 << endl;
+    float discount = 0.7;
+    cout << "Cost of pen " << 40 * discount << endl;
+    cout << "Cost of pencil " << 20 * discount << endl;
+    cout << "Cost of eraser " << 10 * discount << endl;
     return 0;
 }
